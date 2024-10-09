256082c994c821ec44d043eea9c605a7be8096d6
cse18107 <cse18107@cemk.ac.in>
2024-10-09 21:45:15 +0530

    added dotenv

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-
+const dotenv = require('dotenv');
 const app = express();
-
+dotenv.config();
 app.get('/home', (req, res) => {
     res.json({
         message: 'Hello world'
     })
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Started the server");
 })
